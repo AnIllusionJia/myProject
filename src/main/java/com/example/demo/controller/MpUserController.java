3e9c8b29d47c54355894ebacd3cbfd287040071f
myshengzi <18392153115@163.com>
2019-10-23 11:16:42 +0800

    添加baseController

--- src/main/java/com/example/demo/controller/MpUserController.java
+++ src/main/java/com/example/demo/controller/MpUserController.java
@@ -1,9 +1,9 @@
 package com.example.demo.controller;
 
-
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.demo.controller.base.BaseController;
 import com.example.demo.entity.MpUser;
 import com.example.demo.service.MpUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -22,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/mp-user")
-public class MpUserController {
+public class MpUserController extends BaseController {
 
     @Autowired
     private MpUserService mpUserService;
@@ -70,10 +72,20 @@ public class MpUserController {
         mpUserService.removeById(id);
         return "del";
     }
-    public void sss(){
-        int pageNum = 1;
-        int pageSize = 10;
-        IPage<MpUser> mpUserIPage = mpUserService.page(new Page<>(pageNum, pageSize), new QueryWrapper<MpUser>().eq("openid", "openId"));
+
+
+    public void PageList(){
+        //添加条件 查询
+        QueryWrapper<MpUser> query =    new QueryWrapper<MpUser>();
+        query.eq("字段名","字段值");
+        //根据条件分页
+        //IPage<MpUser> mpUserIPage = mpUserService.page(new Page<>(pageNum, pageSize),query);
+        //查询全部分页
+        IPage<MpUser> mpUserIPage = mpUserService.page(new Page<>(pageNum, pageSize));
+        List<MpUser> list =   mpUserIPage.getRecords();
+        list.forEach(mp ->{
+            System.out.println(mp.getId());
+        });
 
     }
 
---
+++ src/main/java/com/example/demo/controller/base/BaseController.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller.base;
+
+public class BaseController {
+    //分页起始页
+    protected  int pageNum = 1;
+    //分页结束页
+    protected  int pageSize = 10;
+
+
+
+}
